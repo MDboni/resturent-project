a9045f6384c00c26ec70aa5ea7bf135befcaf3d2
MDboni <aminboni048@gmail.com>
2025-07-19 21:07:25 +0600

    first commit

---
+++ src/Pages/HomePage/Banner/Banner.jsx
@@ -0,0 +1,49 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import { Carousel } from 'react-responsive-carousel';
+
+import img1 from '../../../assets/home/01.jpg'
+import img2 from '../../../assets/home/02.jpg'
+import img3 from '../../../assets/home/03.png'
+import img4 from '../../../assets/home/04.jpg'
+import img5 from '../../../assets/home/05.png'
+import img6 from '../../../assets/home/06.png'
+
+const Banner = () => {
+  return (
+    <div className="h-[40vh] md:h-[80vh] m-0 p-0 ">
+        <Carousel 
+        autoPlay
+        infiniteLoop
+        interval={3000}
+        showThumbs={false}
+        showStatus={false}
+        showArrows={true}
+        swipeable={true}
+        dynamicHeight={false}
+    >
+    
+           <div>
+            <img src={img1} alt="" className="h-[40vh] md:h-[80vh] w-full object-contain md:object-cover" />
+        </div>
+        <div>
+            <img src={img2} alt="" className="h-[40vh] md:h-[80vh] w-full object-contain md:object-cover"/>
+        </div>
+        <div>
+            <img src={img3} alt="" className="h-[40vh] md:h-[80vh] w-full object-contain md:object-cover"/>
+        </div>
+        <div>
+            <img src={img4} alt="" className="h-[40vh] md:h-[80vh] w-full object-contain md:object-cover"/>
+        </div>
+        <div>
+            <img src={img5} alt="" className="h-[40vh] md:h-[80vh] w-full object-contain md:object-cover"/>
+        </div>
+        <div>
+            <img src={img6} alt="" className="h-[40vh] md:h-[80vh] w-full object-contain md:object-cover"/>
+        </div>
+    
+    </Carousel>
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,16 @@
+import Footer from "../../Layout/Footer"
+import Navbar from "../../Layout/Navbar"
+import Banner from "./Banner/Banner"
+
+
+const HomePage = () => {
+  return (
+    <Navbar>
+       <Banner/>
+       <Footer/>
+         
+    </Navbar>
+  )
+}
+
+export default HomePage
